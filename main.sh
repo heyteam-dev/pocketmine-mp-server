ce61e669a670c7c25a1fa02b403057cb150d51c5
BM_X_Coder <54219181+AbgegriftDH@users.noreply.github.com>
2021-05-11 08:12:47 +0200

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,10 @@
+echo pls wait...
+sleep 4
+echo made by BM_X_Coder
+echo github: https://github.com/AbgegriftDH/pocketmine-mp-server
+echo discord: BM_X_Coder
+echo minecraft: BMXCoder1820
+sleep 5
+echo server starting...
+sleep 2
+bash start.sh
